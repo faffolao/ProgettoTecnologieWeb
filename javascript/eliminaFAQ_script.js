4a47ed1591ebcc5ed701a5d5f79d3a92e0c6dce1
GabrielP <S1097612@studenti.univpm.it>
2023-05-05 17:04:48 +0200

    Implementazione HTML, CSS, JS parziale delle pagine di livello 3

---
+++ javascript/eliminaFAQ_script.js
@@ -0,0 +1,9 @@
+// Aggiungi un listener click su tutti i bottoni "Elimina"
+		const deleteButtons = document.querySelectorAll('.delete-btn');
+		deleteButtons.forEach(button => {
+			button.addEventListener('click', function() {
+				// Elimina la riga corrispondente
+				const row = button.parentNode.parentNode;
+				row.parentNode.removeChild(row);
+			});
+		});
