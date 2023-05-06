00f6f6c447b1f7ccc36e2fcf22ab7e3fc838c5d4
tosca <s1098079@studenti.univpm.it>
2023-05-06 08:56:43 +0200

    Pagina dell'eliminazione delle offerte

---
+++ javascript/elimina_offerte_script.js
@@ -0,0 +1,8 @@
+const deleteButtons = document.querySelectorAll('.delete-btn');
+deleteButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        // Elimina la riga corrispondente
+        const row = button.parentNode.parentNode;
+        row.parentNode.removeChild(row);
+    });
+});
