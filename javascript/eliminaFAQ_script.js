737cb979fbd99266534e9ec6bf6b594453a4a374
GabrielP <S1097612@studenti.univpm.it>
2023-05-08 09:43:55 +0200

    Ridenominazione corretta dei vari files

--- javascript/eliminaFAQ_script.js
+++ javascript/eliminaFAQ_script.js
@@ -1,9 +1,9 @@
 // Aggiungi un listener click su tutti i bottoni "Elimina"
-		const deleteButtons = document.querySelectorAll('.delete-btn');
-		deleteButtons.forEach(button => {
-			button.addEventListener('click', function() {
-				// Elimina la riga corrispondente
-				const row = button.parentNode.parentNode;
-				row.parentNode.removeChild(row);
-			});
-		});
+const deleteButtons = document.querySelectorAll('.delete-btn');
+deleteButtons.forEach(button => {
+	button.addEventListener('click', function() {
+		// Elimina la riga corrispondente
+		const row = button.parentNode.parentNode;
+		row.parentNode.removeChild(row);
+	});
+});
--- javascript/login_script.js
+++ javascript/login_script.js
@@ -6,13 +6,13 @@ function login() {
   // Controlla se l'utente ha inserito l'username e la password corretti
   if (username === "admin" && password === "admin") {
     // Redirect alla pagina desiderata
-    window.location.href = "../html/hub_amministratore.html";
+    window.location.href = "../html/hubAmministratore.html";
   } else if (username === "staff" && password === "staff") {
     // Redirect alla pagina desiderata
-    window.location.href = "../html/hub_staff.html";
+    window.location.href = "../html/hubStaff.html";
   } else if (username === "utente" && password === "utente") {
     // Redirect alla pagina desiderata
-    window.location.href = "../html/hub_utente.html";
+    window.location.href = "../html/hubUtente.html";
   } else {
     // Visualizza un messaggio di errore
     alert("Username o password errati!");
