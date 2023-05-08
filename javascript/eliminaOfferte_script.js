087d637d31978f7f87a3d9257c18c22859772f45
GabrielP <S1097612@studenti.univpm.it>
2023-05-08 14:32:05 +0200

    Correzioni termini

--- javascript/eliminaOfferte_script.js
+++ javascript/eliminaOfferte_script.js
@@ -1,8 +1,21 @@
 const deleteButtons = document.querySelectorAll('.delete-btn');
+
+// deleteButtons.forEach(button => {
+//     button.addEventListener('click', function() {
+//         // Elimina la riga corrispondente
+//         const row = button.parentNode.parentNode;
+//         row.parentNode.removeChild(row);
+//     });
+// });
+
 deleteButtons.forEach(button => {
-    button.addEventListener('click', function() {
-        // Elimina la riga corrispondente
-        const row = button.parentNode.parentNode;
-        row.parentNode.removeChild(row);
-    });
+	button.addEventListener('click', function() {
+		// Elimina la riga corrispondente
+		if (confirm("Vuoi davvero eliminare questa offerta?")){
+			const row = button.parentNode.parentNode;
+			row.parentNode.removeChild(row)}
+		else {
+			return true
+		}
+	});
 });
