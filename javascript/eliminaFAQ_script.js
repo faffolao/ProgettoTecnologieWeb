9610d537765497f9f8fe5a2c52fe33efbc80a690
GabrielP <S1097612@studenti.univpm.it>
2023-05-08 09:46:14 +0200

    Implementazione di un metodo migliore per eliminaFAQ_script.js

--- javascript/eliminaFAQ_script.js
+++ javascript/eliminaFAQ_script.js
@@ -1,9 +1,22 @@
 // Aggiungi un listener click su tutti i bottoni "Elimina"
 const deleteButtons = document.querySelectorAll('.delete-btn');
+
+// deleteButtons.forEach(button => {
+// 	button.addEventListener('click', function() {
+// 		// Elimina la riga corrispondente
+// 		const row = button.parentNode.parentNode;
+// 		row.parentNode.removeChild(row);
+// 	});
+// });
+
 deleteButtons.forEach(button => {
 	button.addEventListener('click', function() {
 		// Elimina la riga corrispondente
-		const row = button.parentNode.parentNode;
-		row.parentNode.removeChild(row);
+		if (confirm("Vuoi davvero eliminare questa domanda?")){
+			const row = button.parentNode.parentNode;
+			row.parentNode.removeChild(row)}
+		else {
+			return true
+		}
 	});
 });
