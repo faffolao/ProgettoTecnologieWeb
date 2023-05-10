a6bb576b68902b6937cdcb32f2eb4e7641692772
GabrielP <S1097612@studenti.univpm.it>
2023-05-10 10:16:56 +0200

    Finite pagine livello 1

---
+++ javascript/listaCouponUtilizzati_script.js
@@ -0,0 +1,19 @@
+function search() {
+  var input = document.getElementById("search-bar");
+  var filter = input.value.toUpperCase();
+  var elements = document.getElementsByClassName("card");
+
+
+  for (var i = 0; i < elements.length; i++) {
+    var text = elements[i].textContent || elements[i].innerText;
+    if (text.toUpperCase().indexOf(filter) > -1) {
+      elements[i].style.display = "";
+    } else {
+      elements[i].style.display = "none";
+    }
+  }
+}
+
+function openPage() {
+  window.location.href = "coupon.html";
+}
