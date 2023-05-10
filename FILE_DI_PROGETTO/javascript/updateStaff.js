6741c7b8aad566c95287954923593d39acc678f7
GabrielP <S1097612@studenti.univpm.it>
2023-05-10 13:00:28 +0200

    Corretto Livello 3 Ora mancano solo le statistiche

--- FILE_DI_PROGETTO/javascript/updateStaff.js
+++ FILE_DI_PROGETTO/javascript/updateStaff.js
@@ -1,11 +1,25 @@
-// Aggiungi un listener click su tutti i bottoni "Elimina"
-const updateButtons = document.querySelectorAll('.update-btn');
+// // Aggiungi un listener click su tutti i bottoni "Elimina"
+// const updateButtons = document.querySelectorAll('.update-btn');
+//
+// updateButtons.forEach(button => {
+//     button.addEventListener('click', function() {
+//         var stringa = document.getElementById("tabella").rows[1].cells[0]; //omar
+//         var nome = prompt("Inserisci il tuo nome:");
+//         // Modifica la riga corrispondente
+//         stringa.innerHTML = nome
+//     });
+// });
 
-updateButtons.forEach(button => {
-    button.addEventListener('click', function() {
-        var stringa = document.getElementById("tabella").rows[1].cells[0]; //omar
-        var nome = prompt("Inserisci il tuo nome:");
-        // Modifica la riga corrispondente
-        stringa.innerHTML = nome
-    });
+const deleteButtons = document.querySelectorAll('.delete-btn');
+
+deleteButtons.forEach(button => {
+	button.addEventListener('click', function() {
+		// Elimina la riga corrispondente
+		if (confirm("Vuoi davvero eliminare questa offerta?")){
+			const row = button.parentNode.parentNode;
+			row.parentNode.removeChild(row)}
+		else {
+			return true
+		}
+	});
 });
