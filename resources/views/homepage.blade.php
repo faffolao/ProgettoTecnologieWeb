a445054ce673ec96d2e0d8895aece9865ff5f28d
Federico Arduini <s1098699@studenti.univpm.it>
2023-05-13 10:07:51 +0200

    risoluzione errorino e aggiunta link ai dettagli di un'azienda in homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -26,49 +26,9 @@
             @foreach($Aziende as $aziende)
                 <div class="card">
                     <img src="data:image/png/jpeg;base64,{{ base64_encode($aziende['logo']) }}" alt="Service Image" class="logo-azienda">
-                    <a class="card-title-link" href="#">{{$aziende['nome']}}</a>
+                    <a class="card-title-link" href="{{ route('dettagliAzienda') }}">{{$aziende['nome']}}</a>
                 </div>
             @endforeach
-{{--            <div class="card">--}}
-{{--                <img src="{{ asset('assets/images/logo_adidas.png') }}" alt="Service Image" class="logo-azienda">--}}
-{{--                <a class="card-title-link" href="#">Adidas</a>--}}
-{{--            </div>--}}
-{{--            <div class="card">--}}
-{{--                <img src="{{ asset('assets/images/default_logo.png') }}" alt="Service Image">--}}
-{{--                <a class="card-title-link" href="#">Apple</a>--}}
-{{--            </div>--}}
-{{--            <div class="card">--}}
-{{--                <img src="https://seeklogo.com/images/M/microsoft-logo-8EE94BD68A-seeklogo.com.png" alt="Service Image">--}}
-{{--                <a class="card-title-link" href="#">Microsoft</a>--}}
-{{--            </div>--}}
-{{--            <div class="card">--}}
-{{--                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Dyson_logo.svg/2560px-Dyson_logo.svg.png" alt="Service Image">--}}
-{{--                <a class="card-title-link" href="#">Dyson</a>--}}
-{{--            </div>--}}
-{{--            <div class="card">--}}
-{{--                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Samsung_Logo.svg/2560px-Samsung_Logo.svg.png" alt="Service Image">--}}
-{{--                <a class="card-title-link" href="#">Samsung</a>--}}
-{{--            </div>--}}
-{{--            <div class="card">--}}
-{{--                <img src="https://upload.wikimedia.org/wikipedia/commons/0/03/Lenovo_Global_Corporate_Logo.png" alt="Service Image">--}}
-{{--                <a class="card-title-link" href="#">Lenovo</a>--}}
-{{--            </div>--}}
-{{--            <div class="card">--}}
-{{--                <img src="https://upload.wikimedia.org/wikipedia/en/0/05/Myprotein_logo.png" alt="Service Image">--}}
-{{--                <a class="card-title-link" href="#">MyProtein</a>--}}
-{{--            </div>--}}
-{{--            <div class="card">--}}
-{{--                <img src="https://www.orafoleo.it/wp-content/uploads/2020/06/danielwellington_logo.png" alt="Service Image">--}}
-{{--                <a class="card-title-link" href="#">Daniel Wellington</a>--}}
-{{--            </div>--}}
-{{--            <div class="card">--}}
-{{--                <img src="https://1000marche.net/wp-content/uploads/2021/01/Just-Eat-logo.png" alt="Service Image">--}}
-{{--                <a class="card-title-link" href="#">Just Eat</a>--}}
-{{--            </div>--}}
-{{--            <div class="card">--}}
-{{--                <img src="https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Huawei_Standard_logo.svg/2016px-Huawei_Standard_logo.svg.png" alt="Service Image">--}}
-{{--                <a class="card-title-link" href="#">Huawei</a>--}}
-{{--            </div>--}}
         </div>
 
         <h2>F.A.Q.</h2>
