1a2bf61c12a844890db090850b183e34f2de6b8d
GabrielP <S1097612@studenti.univpm.it>
2023-05-19 10:29:30 +0200

    Correzione indentazione (phpStorm stupido)

--- public/assets/js/searchOfferta.js
+++ public/assets/js/searchOfferta.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-        var searchInput = document.getElementById('search-bar');
-        var searchForm = document.getElementById('search-form');
+    var searchInput = document.getElementById('search-bar');
+    var searchForm = document.getElementById('search-form');
 
-        searchInput.addEventListener('keydown', function(event) {
-            if (event.key === "Enter") {
-                event.preventDefault();
-                searchForm.submit();
-            }
-        });
+    searchInput.addEventListener('keydown', function(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            searchForm.submit();
+        }
     });
+});
 
 // function search() {
 //     var input = document.getElementById("search-bar");
