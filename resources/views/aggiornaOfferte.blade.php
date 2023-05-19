e282d12943b5c4372c487173e3b367a0f70b0399
tosca <s1098079@studenti.univpm.it>
2023-05-19 17:52:45 +0200

    CRUD offerte

--- resources/views/aggiornaOfferte.blade.php
+++ resources/views/aggiornaOfferte.blade.php
@@ -2,20 +2,42 @@
 
 @section('content')
     <div class="container">
-        <div class="panel">
+        <div class="ins-off ins-off-inputdialog">
             <h2>Aggiorna Offerte</h2>
             <br>
             <form class="form-insertFAQ" action={{url('/aggiornaOfferte/'.$dati['id'])}} method="POST">
                 @csrf
                 @method('PUT')
-                <label for="nome">Aggiorna il nome:</label>
-                <input type="text" id="nome" name="nome" value="{{$dati['nome']}}" required>
+                <div class="form-row">
+                    <div class="form-left">
+                        <fieldset title="Aggiorna dati offerta">
+                            <label for="nome">Aggiorna il nome:</label>
+                            <input type="text" id="nome" name="nome" value="{{$dati['nome']}}" required>
 
-                <label for="oggetto">Aggiorna l'oggetto:</label>
-                <input type="text" id="oggetto" name="oggetto" value="{{$dati['oggetto']}}" required>
+                            <label for="oggetto">Aggiorna l'oggetto:</label>
+                            <input type="text" id="oggetto" name="oggetto" value="{{$dati['oggetto']}}" required>
 
-                <label for="modalitaFruizione">Modalità di fruizione:</label>
-                <input type="text" id="modalitaFruizione" name="modalitaFruizione" value="{{$dati['modalitaFruizione']}}" required>
+                            <label for="modalitaFruizione">Modalità di fruizione:</label>
+                            <input type="text" id="modalitaFruizione" name="modalitaFruizione" value="{{$dati['modalitaFruizione']}}" required>
+                        </fieldset>
+                    </div>
+
+                    <div class="form-right">
+                        <fieldset title="Inserisci dati offerta">
+                            <label for="luogoFruizione">Luogo di fruizione:</label>
+                            <input type="text" id="luogoFruizione" name="luogoFruizione" required>
+
+                            <label for="data_ora_scadenza">Data e ora di scadenza:</label>
+                            <input type="date" id="data_ora_scadenza" name="data_ora_scadenza" required>
+
+                            <fieldset title="Carica immagini in formato .png o .jpeg">
+                                <label for="logo">Carica l'immagine dell'offerta:</label>
+                                <input type="file" id="logo" name="logo"
+                                       accept="image/png, image/jpeg" required>
+                            </fieldset>
+                        </fieldset>
+                    </div>
+                </div>
 
                 <button class="btn" type="submit">Aggiorna l'offerta</button>
             </form>
