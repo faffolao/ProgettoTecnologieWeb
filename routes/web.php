02b13731efdcfd73a79e221950ccea09be55f62c
Federico Arduini <s1098699@studenti.univpm.it>
2023-05-25 08:47:14 +0200

    fix rotte di login mancanti

--- routes/web.php
+++ routes/web.php
@@ -28,21 +28,9 @@ use App\Http\Controllers\LoginController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-/*
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
+// Inclusione file delle rotte per l'autenticazione
 require __DIR__.'/auth.php';
-*/
-
-/*********************************************************************/
 
 Auth::user();
 Auth::id();
