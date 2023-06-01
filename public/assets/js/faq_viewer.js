322059e7ac198342e815b8a7d9fb11da2433b579
Federico Arduini <s1098699@studenti.univpm.it>
2023-06-01 11:51:32 +0200

    riorganizzazione routes e inflating jQuery su alcuni file JS

--- public/assets/js/faq_viewer.js
+++ public/assets/js/faq_viewer.js
@@ -1,14 +1,9 @@
-var questions = document.querySelectorAll(".toggle-list-mini .toggle");
-
-questions.forEach(function(question) {
-    question.addEventListener("click", function() {
-        var answer = this.nextElementSibling;
-        var isShowing = answer.classList.contains("show");
-
-        if (isShowing) {
-            answer.classList.remove("show");
-        } else {
-            answer.classList.add("show");
-        }
+// Quando il DOM è pronto...
+$(document).ready(function() {
+    // Ogni volta che faccio click su un elemento che ha quelle classi li...
+    $(".toggle-list-mini .toggle").click(function() {
+        // Prendo l'elemento che è stato cliccato, ottengo l'elemento che lo segue nel wrapped set, e lo espando/contraggo
+        // a seconda del fatto che è già espanso/contratto (jQuery gestisce autonomamente questo evento con i metodi Toggle)
+        $(this).next().slideToggle();
     });
 });
