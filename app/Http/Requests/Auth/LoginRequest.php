e54fa0577bce3433367f75b33fcc0d8a5ee8f140
Federico Arduini <s1098699@studenti.univpm.it>
2023-06-07 22:52:54 +0200

    piccolo dettaglio

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -46,12 +46,10 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-//        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
-        if (! Auth::attempt($this->only('username', 'password'), $this->boolean('remember'))) {
+        if (! Auth::attempt($this->only('username', 'password'))) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-//                'email' => trans('auth.failed'),
                 'username' => trans('auth.failed'),
             ]);
         }
